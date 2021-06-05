d61b87941637f47b9e137bdada1c21d2cf073f30
chakroid <chakkystark@gmail.com>
2021-06-05 16:54:00 +0530

    db json file

--- lib/components/services.dart
+++ lib/components/services.dart
@@ -1,8 +1,15 @@
 import 'package:http/http.dart' as http;
 import 'players.dart';
 
+
 class Services{
-static const String url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&gpssearch=Sachin+T&gpslimit=10';
+  static const String url = 'https://en.wikipedia.org/w/api.php?action=query&format=json&prop=pageimages%7Cpageterms&generator=prefixsearch&redirects=1&formatversion=2&piprop=thumbnail&pithumbsize=50&pilimit=10&wbptterms=description&gpssearch=Sachin+T&gpslimit=10';
 
+ static Future<List<Player>> getPlayers() async{
 
+   final response =
+      await http.get(Uri.http());
+
+return <Player>[];
+ }
 }
